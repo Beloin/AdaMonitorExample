5d5ec9e8fff674e661e1c548562386b7da647e39
Beloin Sena <beloin.rodrigues@gmail.com>
2023-05-03 22:13:50 -0300

    Trying to use signal and wait in Ada language

--- src/buffermonitored.adb
+++ src/buffermonitored.adb
@@ -1,10 +1,40 @@
 with Ada.Text_IO;
 
 package body BufferMonitored is
+
+    protected type Lock is
+        entry Lock;
+
+        procedure Unlock;
+
+    private
+        Locked : Boolean := False;
+    end Lock;
+
+    protected body Lock is
+        entry Lock when not Locked is
+            use Ada.Text_IO;
+        begin
+            Put_Line ("Locked.");
+            Locked := True;
+        end Lock;
+
+        procedure Unlock is
+            use Ada.Text_IO;
+        begin
+            Put_Line ("Unlocked.");
+            Locked := False;
+        end Unlock;
+    end Lock;
+
+    locker : Lock;
+
     procedure append (i : Integer) is
         use Ada.Text_IO;
     begin
+        locker.Lock;
         Put_Line ("Appended {0}");
+        locker.Unlock;
     end append;
 
     procedure take (i : Integer) is
